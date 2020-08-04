33be00159e79763987dd4e3d5ee9dca15e9b40d9
UsmanHaiderKhan4 <usmanhaiderkhan4@gmail.com>
2020-08-04 22:51:11 +0500

    initial Commit!

---
+++ src/app/logging.service.ts
@@ -0,0 +1,5 @@
+export class LoggingService {
+  loggingStatusChange(status: string) {
+    console.log('Server Status Has Been Changed ,New Status is ' + status);
+  }
+}
---
+++ src/app/new-account/new-account/new-account.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { LoggingService } from '../../logging.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService],
+})
+export class NewAccountComponent implements OnInit {
+  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
+  constructor(private loggingService: LoggingService) {}
+
+  ngOnInit(): void {}
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountAdded.emit({
+      name: accountName,
+      status: accountStatus,
+    });
+    this.loggingService.loggingStatusChange(accountStatus);
+    var service = new LoggingService();
+  }
+}
